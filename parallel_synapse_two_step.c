5dacc1d5cedff4abb596a419e4ffd85d1f2fa6e0
Yuru Song <85913279+london-and-tequila@users.noreply.github.com>
2024-01-08 11:46:35 -0800

    Add files via upload

--- parallel_synapse_two_step.c
+++ parallel_synapse_two_step.c
@@ -4,7 +4,6 @@
 #include <math.h>
 #include <time.h>
 #include <omp.h>
-
 /*
 sum the elements of an int array
 */
@@ -30,7 +29,6 @@ double sum_double(double arr[], int n)
 
     return sum;
 }
-
 /*
 find the minimum value and its index in a double array, return a struct MinId
 */
@@ -39,7 +37,6 @@ struct MinId
     float yval;
     int id;
 };
-
 struct MinId findmin(double arr[], int len)
 {
     struct MinId minid;
@@ -47,6 +44,7 @@ struct MinId findmin(double arr[], int len)
     minid.id = 0;
     for (int c = 1; c < len; c++)
     {
+
         if (arr[c] < minid.yval)
         {
             minid.yval = arr[c];
@@ -56,7 +54,6 @@ struct MinId findmin(double arr[], int len)
 
     return minid;
 };
-
 /*
 define a struct ArraySize to store the size of each array
 */
@@ -84,22 +81,18 @@ Parameters:
 int main(int argc, char *argv[])
 {
     int trial_id = atoi(argv[3]);
-    srand(trial_id); // set the seed for random number generator to trial_id
+    srand(trial_id);
     struct ArraySize array_size;
     const int N = atoi(argv[1]);
     const int P = atoi(argv[2]);
-
     const double Marg = 0.000001;
     const int MaxIter = 10000;
-
     printf("N%d P %d Trial %d\n", N, P, trial_id);
-
     struct MinId minid;
     int *Labels = (int *)malloc(sizeof(int) * (P));
-
     double *Weights = (double *)malloc(sizeof(double) * (P));
     double *Rate = (double *)malloc(sizeof(double) * (P));
-
+    printf("%ld\n", sizeof(int));
     array_size.Labels = (sizeof(int) * (P) / sizeof(int));
     array_size.Weights = (sizeof(double) * (P) / sizeof(int));
     array_size.Rates = (sizeof(double) * (P) / sizeof(int));
@@ -143,7 +136,6 @@ int main(int argc, char *argv[])
     OrdrMat = (int **)malloc(sizeof(int *) * N);
     IOrdrMat = (int **)malloc(sizeof(int *) * N);
     ActvMat = (double **)malloc(sizeof(double *) * N);
-
     for (int i = 0; i < N;)
     {
         OrdrMat[i] = (int *)malloc(sizeof(int) * P);
@@ -162,6 +154,8 @@ int main(int argc, char *argv[])
     array_size.TotAVec = (sizeof(double) * P / sizeof(int)) + sizeof(double *) / sizeof(int);
     array_size.Errors = (sizeof(int) * P / sizeof(int)) + sizeof(double *) / sizeof(int);
 
+    // BEGIN memory usage by variable
+
     printf("array_size.Weights %d kB\n", array_size.Weights / 256);
     printf("array_size.Labels %d kB\n", array_size.Labels / 256);
     printf("array_size.Rates %d kB\n", array_size.Rates / 256);
@@ -173,7 +167,7 @@ int main(int argc, char *argv[])
     printf("array_size.TotAVec %d kB\n", array_size.TotAVec / 256);
 
 #pragma omp parallel for
-    // set the initial values of Weights, Rate, and Labels
+ // set the initial values of Weights, Rate, and Labels
     for (int i = 0; i < P; i++)
     {
         Weights[i] = 1;
@@ -195,6 +189,7 @@ int main(int argc, char *argv[])
         }
         for (int j = 0; j < P; j++)
         {
+
             int k, t;
             k = rand() % (P - j) + j;
             t = Ordr[k];
@@ -208,260 +203,262 @@ int main(int argc, char *argv[])
             OrdrMat[i][j] = Ordr[j];
             IOrdrMat[i][j] = IOrdr[j];
         }
+    }
 
-        free(Ordr);
-        free(IOrdr);
-        end = clock();
-        cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-        printf("generate OrdrMat took %f seconds \n", cpu_time_used);
-        old_cpu_time_used = cpu_time_used;
+    free(Ordr);
+    free(IOrdr);
+    end = clock();
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("generate OrdrMat took %f seconds \n", cpu_time_used);
+    old_cpu_time_used = cpu_time_used;
 #pragma omp parallel for
+    for (int i = 0; i < N; i++)
+    {
+
+        memset(ActvMat[i], 0, sizeof(double) * (P));
+    }
+    for (int k = 0; k < P; k++)
+        TotAVec[k] = (double)0;
+    // start two-step algorithm
+    int j = 0;
+    for (j = 0; j < MaxIter; j++)
+    {
+
+        printf("--------------------------------\n");
+        printf("iter %d \n", j);
+        // for each input dimension, compute activation from envolop function
+            
         for (int i = 0; i < N; i++)
         {
+            memset(Walk, 0, sizeof(double) * (P + 1));
+            oldx = P;
+            newx = P;
+            yval = 0;
+            count = 0;
+            #pragma omp parallel for
+            for (int k = P - 1; k >= 0; k--)
+            {
+                Walk[P - 1 - k] = -(double)Weights[OrdrMat[i][k]] * (double)Labels[OrdrMat[i][k]];
+            }
+            for (int p = 1; p < P;)
+            {
+                Walk[p] = Walk[p - 1] + Walk[p];
+                p++;
+            }
+            double *tWalk = malloc(sizeof(double) * (P));
+#pragma omp parallel for
+            for (int p = 0; p < P; p++)
+            {
+                tWalk[p] = Walk[P - 1 - p];
+            }
+            memcpy(Walk, tWalk, sizeof(double) * (P + 1));
+            free(tWalk);
 
-            memset(ActvMat[i], 0, sizeof(double) * (P));
+            Walk[P] = 0;
+
+            do
+            {
+
+                MeasVec = malloc(sizeof(double) * (oldx));
+                if (MeasVec == NULL)
+                    printf("Fail to allocate MeasVec");
+                MeasVec2 = malloc(sizeof(double) * (oldx));
+                if (MeasVec2 == NULL)
+                    printf("Fail to allocate MeasVec2");
+#pragma omp parallel for
+                for (int k = 0; k < oldx; k++)
+                {
+                    MeasVec2[k] = -(double)(oldx - k) + (Walk[k] - Walk[oldx]);
+                    MeasVec2[k] = MeasVec2[k] / ((double)(oldx - k));
+                }
+
+                minid = findmin(MeasVec2, oldx);
+                yval = minid.yval;
+                newx = minid.id;
+
+                if (yval >= 0)
+                {
+
+                    memset(ActvMat[i], 0, oldx);
+                    break;
+                }
+
+#pragma omp parallel for
+                for (int k = newx; k < oldx; k++)
+                {
+                    ActvMat[i][k] = -yval;
+                }
+
+                oldx = newx;
+
+                count++;
+                end = clock();
+                cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+                if (count % 5000 == 0)
+                    printf("iter %d, n %d, walk %d: %f s, total: %f s\n", j, i, count, cpu_time_used - old_cpu_time_used, cpu_time_used);
+                old_cpu_time_used = cpu_time_used;
+
+                free(MeasVec);
+                free(MeasVec2);
+
+            } while (newx > 0);
         }
+
+#pragma omp parallel for
+// compute the total activation for each data
         for (int k = 0; k < P; k++)
-            TotAVec[k] = (double)0;
-
-        // start two-step algorithm
-        int j = 0;
-        for (j = 0; j < MaxIter; j++)
         {
+            TotAVec[k] = 0;
 
-            printf("--------------------------------\n");
-            printf("iter %d \n", j);
+            for (int n = 0; n < N; n++)
+            {
 
-            // for each input dimension, compute activation from envolop function
+                TotAVec[k] = TotAVec[k] + ActvMat[n][IOrdrMat[n][k]];
+            }
+        }
+
+        end = clock();
+        cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+        printf("ActvMat: %f s, total: %f s\n", cpu_time_used - old_cpu_time_used, cpu_time_used);
+        old_cpu_time_used = cpu_time_used;
+
+        Thresh = sum_double(TotAVec, P);
+        Thresh = Thresh / (double)P;
+#pragma omp parallel for
+ // compute the number of errors
+        for (int k = 0; k < P; k++)
+            Errors[k] = ((TotAVec[k] - Thresh) * (double)Labels[k]) < Marg * Thresh;
+
+        NumError = sum(Errors, P);
+        printf("NumError %d\n ", NumError);
+        // if the number of errors is 0, then the algorithm converges
+            // write the results to files
+        if (NumError == 0)
+        {
+            printf("correct!\n");
+            FLAG = 0;
+            FILE *fp2 = NULL;
+            //    char *filepath = "/Users/yuru/Documents/GitHub/Monsyn/";
+            char filename[100];
+
+            sprintf(filename, "./monsyn4/N%dP%dtrial%d.txt", N, P, trial_id);
+            remove(filename);
+            fp2 = fopen(filename, "aw");
+            if (fp2 == NULL)
+            {
+                printf("Error while opening the file.\n");
+                return -1;
+            }
+            fprintf(fp2, "%d,%d,%d,%d,%d,%d", N, P, trial_id, sum(Errors, P), j, FLAG);
+            fclose(fp2);
+
+            char filename1[100];
+            sprintf(filename1, "./monsyn4/N%dP%dtrial%dinput.txt", N, P, trial_id);
+            remove(filename1);
+            FILE *f = fopen(filename1, "wb");
+            if (f == NULL)
+            {
+                printf("Error while opening the file.\n");
+                return -1;
+            }
             for (int i = 0; i < N; i++)
             {
-                memset(Walk, 0, sizeof(double) * (P + 1));
-                oldx = P;
-                newx = P;
-                yval = 0;
-                count = 0;
-#pragma omp parallel for
-                for (int k = P - 1; k >= 0; k--)
+                for (int k = 0; k < P; k++)
                 {
-                    Walk[P - 1 - k] = -(double)Weights[OrdrMat[i][k]] * (double)Labels[OrdrMat[i][k]];
+                    fprintf(f, "%d ", OrdrMat[i][k]);
                 }
-                for (int p = 1; p < P;)
-                {
-                    Walk[p] = Walk[p - 1] + Walk[p];
-                    p++;
-                }
-                double *tWalk = malloc(sizeof(double) * (P));
-#pragma omp parallel for
-                for (int p = 0; p < P; p++)
-                {
-                    tWalk[p] = Walk[P - 1 - p];
-                }
-
-                memcpy(Walk, tWalk, sizeof(double) * (P + 1));
-                free(tWalk);
-
-                Walk[P] = 0;
-
-                do
-                {
-
-                    MeasVec = malloc(sizeof(double) * (oldx));
-                    if (MeasVec == NULL)
-                        printf("Fail to allocate MeasVec");
-                    MeasVec2 = malloc(sizeof(double) * (oldx));
-                    if (MeasVec2 == NULL)
-                        printf("Fail to allocate MeasVec2");
-
-#pragma omp parallel for
-                    for (int k = 0; k < oldx; k++)
-                    {
-                        MeasVec2[k] = -(double)(oldx - k) + (Walk[k] - Walk[oldx]);
-                        MeasVec2[k] = MeasVec2[k] / ((double)(oldx - k));
-                    }
-
-                    minid = findmin(MeasVec2, oldx);
-                    yval = minid.yval;
-                    newx = minid.id;
-
-                    if (yval >= 0)
-                    {
-
-                        memset(ActvMat[i], 0, oldx);
-                        break;
-                    }
-
-#pragma omp parallel for
-                    for (int k = newx; k < oldx; k++)
-                    {
-                        ActvMat[i][k] = -yval;
-                    }
-                    oldx = newx;
-                    count++;
-                    end = clock();
-                    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-
-                    if (count % 5000 == 0)
-                        printf("iter %d, n %d, walk %d: %f s, total: %f s\n", j, i, count, cpu_time_used - old_cpu_time_used, cpu_time_used);
-                    old_cpu_time_used = cpu_time_used;
-                    free(MeasVec);
-                    free(MeasVec2);
-
-                } while (newx > 0);
+                fprintf(f, "\n");
             }
-#pragma omp parallel for
-            // compute the total activation for each data
-            for (int k = 0; k < P; k++)
+            fclose(f);
+
+            char filename2[100];
+            sprintf(filename2, "./monsyn4/N%dP%dtrial%dfunc.txt", N, P, trial_id);
+            remove(filename2);
+            FILE *f2 = fopen(filename2, "wb");
+            if (f2 == NULL)
             {
-                TotAVec[k] = 0;
-
-                for (int n = 0; n < N; n++)
-                {
-
-                    TotAVec[k] = TotAVec[k] + ActvMat[n][IOrdrMat[n][k]];
-                }
+                printf("Error while opening the file.\n");
+                return -1;
             }
-
-            end = clock();
-            cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-            printf("ActvMat: %f s, total: %f s\n", cpu_time_used - old_cpu_time_used, cpu_time_used);
-            old_cpu_time_used = cpu_time_used;
-
-            Thresh = sum_double(TotAVec, P);
-            Thresh = Thresh / (double)P;
-#pragma omp parallel for
-            // compute the number of errors
-            for (int k = 0; k < P; k++)
-                Errors[k] = ((TotAVec[k] - Thresh) * (double)Labels[k]) < Marg * Thresh;
-
-            NumError = sum(Errors, P);
-            printf("NumError %d\n ", NumError);
-
-            // if the number of errors is 0, then the algorithm converges
-            // write the results to files
-            if (NumError == 0)
+            for (int i = 0; i < N; i++)
             {
-                printf("correct!\n");
-                FLAG = 0;
-                FILE *fp2 = NULL;
-                
-                char filename[100];
-
-                sprintf(filename, "./monsyn4/N%dP%dtrial%d.txt", N, P, trial_id);
-                remove(filename);
-                fp2 = fopen(filename, "aw");
-                if (fp2 == NULL)
+                for (int k = 0; k < P; k++)
                 {
-                    printf("Error while opening the file.\n");
-                    return -1;
+                    fprintf(f2, "%.4f ", ActvMat[i][k]);
                 }
-                fprintf(fp2, "%d,%d,%d,%d,%d,%d", N, P, trial_id, sum(Errors, P), j, FLAG);
-                fclose(fp2);
-
-                char filename1[100];
-                sprintf(filename1, "./monsyn4/N%dP%dtrial%dinput.txt", N, P, trial_id);
-                remove(filename1);
-                FILE *f = fopen(filename1, "wb");
-                if (f == NULL)
-                {
-                    printf("Error while opening the file.\n");
-                    return -1;
-                }
-                for (int i = 0; i < N; i++)
-                {
-                    for (int k = 0; k < P; k++)
-                    {
-                        fprintf(f, "%d ", OrdrMat[i][k]);
-                    }
-                    fprintf(f, "\n");
-                }
-                fclose(f);
-
-                char filename2[100];
-                sprintf(filename2, "./monsyn4/N%dP%dtrial%dfunc.txt", N, P, trial_id);
-                remove(filename2);
-                FILE *f2 = fopen(filename2, "wb");
-                if (f2 == NULL)
-                {
-                    printf("Error while opening the file.\n");
-                    return -1;
-                }
-                for (int i = 0; i < N; i++)
-                {
-                    for (int k = 0; k < P; k++)
-                    {
-                        fprintf(f2, "%.4f ", ActvMat[i][k]);
-                    }
-                    fprintf(f2, "\n");
-                }
-                fclose(f2);
-                break;
+                fprintf(f2, "\n");
             }
+            fclose(f2);
+            break;
+        }
 
 #pragma omp parallel for
-            // update the weights
-            for (int k = 0; k < P; k++)
-            {
-                Rate[k] = Errors[k] * (Rate[k] + 0.2 * Errors[k]);
-                Weights[k] = Weights[k] + Rate[k];
-            }
-            printf("Weights: %f s, total: %f s\n", cpu_time_used - old_cpu_time_used, cpu_time_used);
-        }
-
-        end = clock();
-        cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-        printf("total %f seconds to execute \n", cpu_time_used);
-        old_cpu_time_used = cpu_time_used;
-
-        FILE *fp2 = NULL;
-        
-        char filename[100];
-
-        sprintf(filename, "./monsyn4/N%dP%dtrial%d.txt", N, P, trial_id);
-        remove(filename);
-        fp2 = fopen(filename, "aw");
-        if (fp2 == NULL)
+// update the weights
+        for (int k = 0; k < P; k++)
         {
-            printf("Error while opening the file.\n");
-            return -1;
+            Rate[k] = Errors[k] * (Rate[k] + 0.2 * Errors[k]);
+            Weights[k] = Weights[k] + Rate[k];
+            // printf("Weights[%d], %.4f\n",k, Weights[k]);
         }
-        fprintf(fp2, "%d,%d,%d,%d,%d,%d", N, P, trial_id, sum(Errors, P), j, FLAG);
-        fclose(fp2);
-
-        char filename1[100];
-        sprintf(filename1, "./monsyn4/N%dP%dtrial%dinput.txt", N, P, trial_id);
-        remove(filename1);
-        FILE *f = fopen(filename1, "wb");
-        if (f == NULL)
-        {
-            printf("Error while opening the file.\n");
-            return -1;
-        }
-        for (int i = 0; i < N; i++)
-        {
-            for (int k = 0; k < P; k++)
-            {
-                fprintf(f, "%d ", OrdrMat[i][k]);
-            }
-            fprintf(f, "\n");
-        }
-        fclose(f);
-
-        char filename2[100];
-        sprintf(filename2, "./monsyn4/N%dP%dtrial%dfunc.txt", N, P, trial_id);
-        remove(filename2);
-        FILE *f2 = fopen(filename2, "wb");
-        if (f2 == NULL)
-        {
-            printf("Error while opening the file.\n");
-            return -1;
-        }
-        for (int i = 0; i < N; i++)
-        {
-            for (int k = 0; k < P; k++)
-            {
-                fprintf(f2, "%.4f ", ActvMat[i][k]);
-            }
-            fprintf(f2, "\n");
-        }
-        fclose(f2);
+        printf("Weights: %f s, total: %f s\n", cpu_time_used - old_cpu_time_used, cpu_time_used);
     }
+
+    end = clock();
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("total %f seconds to execute \n", cpu_time_used);
+    old_cpu_time_used = cpu_time_used;
+
+    FILE *fp2 = NULL;
+    //    char *filepath = "/Users/yuru/Documents/GitHub/Monsyn/";
+    char filename[100];
+
+    sprintf(filename, "./monsyn4/N%dP%dtrial%d.txt", N, P, trial_id);
+    remove(filename);
+    fp2 = fopen(filename, "aw");
+    if (fp2 == NULL)
+    {
+        printf("Error while opening the file.\n");
+        return -1;
+    }
+    fprintf(fp2, "%d,%d,%d,%d,%d,%d", N, P, trial_id, sum(Errors, P), j, FLAG);
+    fclose(fp2);
+
+    char filename1[100];
+    sprintf(filename1, "./monsyn4/N%dP%dtrial%dinput.txt", N, P, trial_id);
+    remove(filename1);
+    FILE *f = fopen(filename1, "wb");
+    if (f == NULL)
+    {
+        printf("Error while opening the file.\n");
+        return -1;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        for (int k = 0; k < P; k++)
+        {
+            fprintf(f, "%d ", OrdrMat[i][k]);
+        }
+        fprintf(f, "\n");
+    }
+    fclose(f);
+
+    char filename2[100];
+    sprintf(filename2, "./monsyn4/N%dP%dtrial%dfunc.txt", N, P, trial_id);
+    remove(filename2);
+    FILE *f2 = fopen(filename2, "wb");
+    if (f2 == NULL)
+    {
+        printf("Error while opening the file.\n");
+        return -1;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        for (int k = 0; k < P; k++)
+        {
+            fprintf(f2, "%.4f ", ActvMat[i][k]);
+        }
+        fprintf(f2, "\n");
+    }
+    fclose(f2);
+}
