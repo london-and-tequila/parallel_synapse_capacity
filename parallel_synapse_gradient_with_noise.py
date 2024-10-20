75b4c6e386d85fbcbedd342505cb8a77eb7fecae
london-and-tequila <yus027@ucsd.edu>
2024-10-19 17:11:14 -0700

    start noisy input testing

--- parallel_synapse_gradient_with_noise.py
+++ parallel_synapse_gradient_with_noise.py
@@ -2,16 +2,96 @@ import argparse
 import os
 import time
 
+import numpy as np
 import torch
 
 from parallel_synapse_gradient import ACCURACY_THRESHOLD, ParallelSyn, TrainParallelSyn
 from utils_parallel_syn_gradient import (
     add_noise,
     generate_data_torch,
+    inverse_cdf,
     load_model,
     plot_trial,
     save_model,
 )
+
+
+class TrainParallelSynWithNoise(TrainParallelSyn):
+    """
+    Apart from the regular training, we have three different testing accuracies:
+    - accuracy: the accuracy of the model on the original data, without noise.
+    - accuracy_noisy_train: the accuracy of the model on the noisy data, with noise, different from the training data.
+    - accuracy_noisy_test: the accuracy of the model on the noisy data, with noise, same as the training data.
+    """
+
+    def __init__(self, train_params):
+        super().__init__(train_params)
+
+    def train(self, model: ParallelSyn, data: dict, t1: float):
+        """
+
+        Args:
+            model (ParallelSyn)
+            data (dict): {
+                "input
+            }
+            t1 (float): _description_
+        """
+        # set up optimizer
+        self.optim = torch.optim.Adam(
+            [
+                {"params": model.ampli},
+                {"params": model.slope},
+                {"params": model.theta},
+                {"params": model.thres, "lr": self.threslr},
+            ],
+            lr=self.adamlr,
+        )
+
+        # set up threshold pool, a pool of random thresholds, sample more thresholds from closer to 0 or 1
+        if self.distribution == "uniform":
+            self.thresPool = torch.tensor(
+                inverse_cdf(np.random.uniform(size=(self.NthresPool, 1))),
+                device=model.device,
+                dtype=torch.float32,
+            ).float()
+        elif self.distribution == "gaussian":
+            self.thresPool = torch.tensor(
+                np.random.normal(size=(self.NthresPool, 1)),
+                device=model.device,
+                dtype=torch.float32,
+            ).float()
+
+        for k in range(self.Nepoch):
+            self.shuffle_invalid(model)
+
+            with torch.no_grad():
+                # clamp the slope to be non-negative
+                model.slope.clamp_min_(0)
+
+            model.forward(inputX)
+            self.lossFunc(model, label)
+            self.loss.backward()
+            self.optim.step()
+            self.optim.zero_grad()
+
+            model.forward(inputX)
+            self.accu(model, label)
+
+            # record the loss and accuracy every downSample epochs
+            if (k % self.downSample) == 5:
+                if len(self.acc_history) > self.maxRecord * self.downSample:
+                    self.acc_history.popleft()
+                    self.loss_history.popleft()
+                    self.time.popleft()
+                self.acc_history.append(self.acc.detach())
+                self.loss_history.append(self.loss.detach())
+                self.time.append(time.time() - t1)
+
+            if self.acc > ACCURACY_THRESHOLD:
+                print("accuracy reached 1")
+                break
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -73,6 +153,7 @@ if __name__ == "__main__":
     )
 
     # create folder to save the model and load the model if it exists
+    # TODO: modify below for dict input
     if os.path.isfile(folder + "/" + path + "_data") and os.path.isfile(
         folder + "/" + path
     ):
@@ -92,6 +173,8 @@ if __name__ == "__main__":
     # create new model if it does not exist
     else:
         print("creating new model")
+
+        # original data
         inputX, label = generate_data_torch(
             nDimension=model_params["N"],
             nSample=train_params["P"],
@@ -99,9 +182,15 @@ if __name__ == "__main__":
             device=model_params["device"],
             distribution=model_params["distribution"],
         )
-        inputX, label = add_noise(
+        # noisy training data
+        inputX_noisy_train, label_noisy_train = add_noise(
             inputX, label, noise_size=args.noise_size, noise_repeat=args.noise_repeat
         )
+        # noisy testing data
+        inputX_noisy_test, label_noisy_test = add_noise(
+            inputX, label, noise_size=args.noise_size, noise_repeat=args.noise_repeat
+        )
+
         path = ""
         path += (
             "N_"
@@ -118,13 +207,26 @@ if __name__ == "__main__":
             + str(args.noise_repeat)
         )
 
+        # saving original data
         data_ = torch.hstack((inputX.cpu(), label.reshape(-1, 1).cpu()))
         save_model(data_, folder + "/" + path + "_data")
+
+        # saving noisy training data
+        data_ = torch.hstack(
+            (inputX_noisy_train.cpu(), label_noisy_train.reshape(-1, 1).cpu())
+        )
+        save_model(data_, folder + "/" + path + "_noisy_train_data")
+
+        # saving noisy testing data
+        data_ = torch.hstack(
+            (inputX_noisy_test.cpu(), label_noisy_test.reshape(-1, 1).cpu())
+        )
+        save_model(data_, folder + "/" + path + "_noisy_test_data")
 
         model = ParallelSyn(model_params)
         model.to(model_params["device"])
 
-    trial = TrainParallelSyn(train_params)
+    trial = TrainParallelSynWithNoise(train_params)
     t1 = time.time()
     for repeat in range(800):
         trial.train(model, label, inputX, t1)
